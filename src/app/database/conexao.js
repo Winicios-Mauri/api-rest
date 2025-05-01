23c91059a4d440c983f7288e90223fd23edf3265
WiniciosMauri <97113996+Winicios-Mauri@users.noreply.github.com>
2025-05-01 20:12:15 -0300

    Feat: Refactory MVC

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -10,4 +10,13 @@ const conexao = mysql.createConnection({
 
 conexao.connect()
 
+export const query = (sql, values='', messageReject) => {
+    return new Promise((resolve, reject) => {
+        conexao.query(sql, values, (error, result) => {
+            if(error) return reject(me)
+            return resolve(JSON.parse(JSON.stringify(result)))
+        })
+    })
+}
+
 export default conexao
--- src/app/repositores/SelecaoRepository.js
+++ src/app/repositores/SelecaoRepository.js
@@ -1,55 +1,30 @@
-import conexao from "../database/conexao.js"
+import { query } from "../database/conexao.js"
 
 class SelecaoRepository {
 
     create(data){
         const sql = "INSERT INTO selecoes SET ?;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, data, (error, result) => {
-                if(error) return reject('Não foi possivel cadastrar')
-                return resolve(JSON.parse(JSON.stringify(result)))
-            })
-        })
+        return query(sql, data, 'Não foi possivel cadastrar')
     }
 
     findAll(){
         const sql = "SELECT * FROM selecoes;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, (error, result) => {
-                if(error) return reject('Não foi possivel localizar')
-                return resolve(JSON.parse(JSON.stringify(result)))
-            })
-        })
+        return query(sql, 'Não foi possivel localizar')
     }
 
     findById(id){
         const sql = "SELECT * FROM selecoes WHERE id=?;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, id, (error, result) => {
-                if(error) return reject('Não foi possivel localizar')
-                return resolve(JSON.parse(JSON.stringify(result)))
-            })
-        })
+        return query(sql, id, 'Não foi possivel localizar')
     }
 
     update(data, id){
         const sql = "UPDATE selecoes SET ? WHERE id =?;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, [data, id], (error, result) => {
-                if(error) return reject('Não foi atualizar o registro')
-                return resolve(JSON.parse(JSON.stringify(result)))
-            })
-        })
+        return query(sql, [data, id], 'Não foi atualizar o registro')
     }
 
     delete(id){
         const sql = "DELETE FROM selecoes WHERE id=?;"
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, id, (error, result) => {
-                if(error) return reject('Não foi deletar o registro')
-                return resolve(JSON.parse(JSON.stringify(result)))
-            })
-        })
+        return query(sql, id, 'Não foi possivel deletar o registro')
     }
 
 
