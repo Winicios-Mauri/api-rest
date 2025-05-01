37b12f256cb4a0f5c98f32676dd7f71477442374
WiniciosMauri <97113996+Winicios-Mauri@users.noreply.github.com>
2025-05-01 20:22:55 -0300

    Feat: Add routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express from 'express';
-import SelecaoController from './app/controllers/SelecaoController.js';
+import routes from './routes.js';
 
 const app = express();
+
+// Usando o Router
+app.use(routes)
 
 // Indicando para o express ler body com json
 app.use(express.json())
 
-app.get('/selecoes', SelecaoController.index)
-app.get('/selecoes/:id', SelecaoController.show)
-app.post('/selecoes', SelecaoController.store)
-app.put('/selecoes/:id', SelecaoController.update)
-app.delete('/selecoes/:id', SelecaoController.delete)
+
 
 export default app
