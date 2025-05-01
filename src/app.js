4940cd367d1746e87b1f85dab6bb98d1da1f820c
WiniciosMauri <97113996+Winicios-Mauri@users.noreply.github.com>
2025-04-30 21:01:43 -0300

    Feat: Data Persistence

--- src/app.js
+++ src/app.js
@@ -1,55 +1,70 @@
 import express from 'express';
+import conexao from '../infra/conexao.js';
 
 const app = express();
 
 // Indicando para o express ler body com json
 app.use(express.json())
 
-// Mock
-const selecoes = [
-    {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Suécia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'},
-]
-
-function buscarSelecaoPorId(id) {
-    return selecoes.filter(selecao => selecao.id == id)
-}
-
-function buscarIndexSeleceao(id) {
-    return selecoes.findIndex(selecao => selecao.id == id)
-}
-
-// Criando a rota padrão
-app.get('/', (req, res) => {
-    res.send('Curso de Node JS')
-})
-
 app.get('/selecoes', (req, res) => {
-    res.status(200).send(selecoes)
+    const sql = "SELECT * FROM selecoes;"
+    conexao.query(sql, (error, result) => {
+        if (error) {
+            res.status(404).json({'erro': error})
+        }else {
+            res.status(200).json(result)
+        }
+    })
 })
 
 app.get('/selecoes/:id', (req, res) => {
-    res.json(buscarSelecaoPorId(req.params.id))
+    const id = req.params.id;
+    const sql = "SELECT * FROM selecoes WHERE id=?;"
+    conexao.query(sql, id, (error, result) => {
+        const line = result[0]
+        if (error) {
+            res.status(404).json({'erro': error})
+        }else {
+            res.status(200).json(line)
+        }
+    })
 })
 
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Seleção Cadastrada com Sucesso!')
+    const data = req.body
+    const sql = "INSERT INTO selecoes SET ?;"
+    conexao.query(sql, data, (error, result) => {
+        if (error) {
+            res.status(404).json({'erro': error})
+        }else {
+            res.status(201).json(result)
+        }
+    })
 })
 
 app.delete('/selecoes/:id', (req, res) => {
-    let index = buscarIndexSeleceao(req.params.id)
-    selecoes.splice(index, 1)
-    res.send(`Seleção com ID ${req.params.id} Excluída com Sucesso!`)
+    const id = req.params.id;
+    const sql = "DELETE FROM selecoes WHERE id=?;"
+    conexao.query(sql, id, (error, result) => {
+        if (error) {
+            res.status(404).json({'erro': error})
+        }else {
+            res.status(200).json(result)
+        }
+    })
 })
 
 app.put('/selecoes/:id', (req, res) => {
-    let index = buscarIndexSeleceao(req.params.id)
-    selecoes[index].selecao = req.body.selecao;
-    selecoes[index].grupo = req.body.grupo;
-    res.json(selecoes[index])
+    const data = req.body
+    const id = req.params.id;
+    const sql = "UPDATE selecoes SET ? WHERE id =?;"
+    conexao.query(sql, [data, id], (error, result) => {
+        if (error) {
+            res.status(400).json({'erro': error})
+        }else {
+            res.status(200).json(result)
+        }
+    })
 })
 
 export default app
